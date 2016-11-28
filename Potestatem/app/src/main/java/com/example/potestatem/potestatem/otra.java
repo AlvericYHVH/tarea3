a2c229a297a04171f9dde21fae3f3728b319f743
AlvericYHVH <yave_333@hotmail.com>
2016-11-27 19:10:04 -0700

    tarea semana 3
    tarea de semana 3 bienvenida

---
+++ Potestatem/app/src/main/java/com/example/potestatem/potestatem/otra.java
@@ -0,0 +1,13 @@
+package com.example.potestatem.potestatem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class otra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otra);
+    }
+}
